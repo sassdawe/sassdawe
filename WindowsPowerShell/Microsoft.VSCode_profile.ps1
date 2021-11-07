66d01c03c01d965e2589618bfb359757d0e758ea
David Sass <elethulyelenseg@hotmail.com>
2021-11-07 20:10:24 +0100

    fix (Profile): VSCode for Windows PowerShell

--- WindowsPowerShell/Microsoft.VSCode_profile.ps1
+++ WindowsPowerShell/Microsoft.VSCode_profile.ps1
@@ -3,6 +3,15 @@ $PSDefaultParameterValues["Out-Default:OutVariable"] = "__"
 $Host.PrivateData.ErrorForegroundColor = "DarkYellow"
 $hosts = "C:\Windows\System32\drivers\etc\hosts"
 
+if ($true) {
+    if ($PSVersionTable.PSVersion.Major -eq 5) {
+        $env:PSModulePath = ($env:PSModulePath.Split(';') | Where-Object { (-not ( $_ -like "*Files\powershell*" ))-and (-not ( $_ -like "*\Powershell\Modules" )) }) -join ';'
+        $env:PSModulePath += ";$(Split-Path $profile)\Modules"
+        Get-Module | Where-Object { $_.path -ilike '*Files\PowerShell*' } | Remove-Module
+    }
+
+}
+
 Set-PSReadLineOption -AddToHistoryHandler {
     param([string]$line)
 
